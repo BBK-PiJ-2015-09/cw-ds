49565f4f262eb8c0e44d639edb3dfe536ab442bd
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-01-02 16:24:51 +0000

    Add get, add(index, item) and add(item) methods to ArrayList, with supporting methods to expand the list when needed

--- ArrayList.java
+++ ArrayList.java
@@ -1,18 +1,18 @@
 public class ArrayList implements List {
 	private Object[] list;
-	private int length;
+	private int objectCount;
 
 	/**
 	 * Constructor
 	 */
 	public ArrayList() {
 		list = new Object[1];
-		length = 0;
+		objectCount = 0;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		if (length == 0) {
+		if (objectCount == 0) {
 			return true;
 		} else {
 			return false;
@@ -21,7 +21,67 @@ public class ArrayList implements List {
 
 	@Override
 	public int size() {
-		return length;
+		return objectCount;
 	}
 
+	@Override
+	public ReturnObject get(int index) {
+		if (index >= list.length || index < 0) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			return new ReturnObjectImpl(list[index]);
+		}
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		if (index >= list.length || index < 0) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if (item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else {
+			list[index] = item;
+			if (objectCount == 0) {
+				objectCount = 1;
+			}
+			return new ReturnObjectImpl();
+		}
+	}
+	
+	@Override
+	public ReturnObject add(Object item) {
+		if (item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else {
+			if (objectCount + 1 >= list.length) {
+				expandList();
+			}
+			list[objectCount] = item;
+			objectCount++;
+			return new ReturnObjectImpl();
+		}
+	}
+	
+	/**
+	 * Doubles the size of the list. Uses more memory than expanding 
+	 * array by 1 each time but faster when adding many objects.
+	 */
+	private void expandList() {
+		Object[] newList = new Object[list.length * 2];
+		copyArray(list, newList);
+		list = newList;
+	}
+	
+	/**
+	 * Copy from one array to another.
+	 * Does not check array size.
+	 *
+	 * @param the Object[] to copy from
+	 * @param the Object[] to copy to
+	 */
+	private void copyArray(Object[] from, Object[] to) {
+		for(int i = 0; i < from.length; i++) {
+			to[i] = from[i];
+		}
+	}
 }
--- ArrayListTest.java
+++ ArrayListTest.java
@@ -4,6 +4,15 @@ public class ArrayListTest {
 		ArrayList list = new ArrayList();
 		System.out.println(list.isEmpty() == true);
 		System.out.println(list.size() == 0);
+		list.add(0, "first object");
+		System.out.println(list.get(0).getReturnValue() == "first object");
+		System.out.println(list.isEmpty() == false);
+		System.out.println(list.size() == 1);
+		System.out.println(list.add(1, "second object").getError() == ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		System.out.println(list.size() == 1);
+		System.out.println(list.add("second object").getReturnValue() == null);
+		System.out.println(list.get(0).getReturnValue() == "first object");
+		System.out.println(list.get(1).getReturnValue() == "second object");
 	}
 
 }
